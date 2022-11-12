b0b91368603c35f370db7c1f78ae930afeb14059
eric macbook pro <lil.reenas@gmail.com>
2022-11-12 12:09:51 +0200

    init

---
+++ Assets/LekcijaDivi.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LekcijaDivi : MonoBehaviour
+{
+
+    private int guesableNumber = 0;
+
+    [SerializeField] private int inputNumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Method7(40);
+        // Method7(15);
+        // Method7(90);
+
+        guesableNumber = Random.Range(0, 100);
+    }
+
+    private void Method1()
+    {
+        if (1 < 2)
+        {
+            Debug.Log("yes");
+        }
+    }
+    
+    private void Method2()
+    {
+        int x = 5;
+        int y = 4;
+        
+        if (x < y)
+        {
+            Debug.Log("yes");
+        }
+    }
+    
+    private void Method3()
+    {
+        int x = 5;
+        int y = 4;
+        
+        if (x < y)
+        {
+            Debug.Log("yes");
+        }
+        else
+        {
+            Debug.Log("no");
+        }
+    }
+    
+    private void Method4()
+    {
+        int x = 5;
+        int y = 4;
+        
+        if (x < y)
+        {
+            Debug.Log("yes");
+        }
+        else if (x == y)
+        {
+            Debug.Log("equal");
+        }
+        else
+        {
+            Debug.Log("no");
+        }
+    }
+
+    private void Method5()
+    {
+        bool canJump = false;
+        bool isGrouned = true;
+
+        if (isGrouned)
+        {
+            canJump = true;
+        }
+
+        if (canJump)
+        {
+            Debug.Log("can");
+        }
+    }
+    
+    private void Method6()
+    {
+        bool canJump = false;
+        bool isGrouned = true;
+
+        if (isGrouned && !canJump)
+        {
+            Debug.Log("can");
+        }
+        if (isGrouned || !canJump)
+        {
+            Debug.Log("can");
+        }
+    }
+
+    private void Method7(int age)
+    {
+        Printsmth(age.ToString());
+        
+        if (age < 18)
+        {
+            Debug.Log("free candy");
+        }
+        else if( age > 70)
+        {
+            Debug.Log("too old");
+        }
+        else
+        {
+            Debug.Log("propper");
+        }
+    }
+
+    private void Printsmth(string smth)
+    {
+        Debug.Log(smth);
+    }
+
+    public void GuessingGame()
+    {
+        if (guesableNumber == inputNumber)
+        {
+            Debug.Log("You won");
+        }
+        else
+        {
+            if (guesableNumber > inputNumber)
+            {
+                Debug.Log("too low");
+            }
+            else
+            {
+                Debug.Log("to high");
+            }
+        }
+    }
+    
+}
+/*
+1. Divisible by 2 or 3
+Given two integers, write a method that returns their multiplication if they are both divisible by 2 or 3, 
+otherwise returns their sum.
+Expected input and output
+DivisibleBy2Or3(15, 30) → 450
+if (( x % 2) == 0) 
+if(( x % 3) == 0 )
+
+
+2. If greater than third one
+Given an array of 3 integers, write a method that checks if multiplication or sum of two first numbers 
+is greater than third one.
+Expected input and output
+IfGreaterThanThirdOne([2, 7, 12]) → true
+
+
+3. If number is even
+Given an integer, write a method that checks if it is even.
+Expected input and output
+IfNumberIsEven(721) → false
+
+--- if(( x % 2 ) == 0)
+*/
+
+// > < >= <= == !=
---
+++ Assets/Lekcija_1.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+
+public class Lekcija_1 : MonoBehaviour
+{
+    // one line comment
+    
+    /*
+     multiple
+     line 
+     comment
+     */
+
+    // variable type / variable name / variable value
+    [SerializeField] private int integer = 0;
+    private string name = "vards";
+    private float flaoter = 0.3f;
+    private bool yesOrNo = true;
+
+    private int a = 1;
+    private int b = 2;
+    private int d = 0;
+    
+    public void Method1() // access type / return type / name
+    {
+        Debug.Log("aaaaaaa");
+        int c = a + b;
+        Debug.Log(c);
+        Debug.Log( "kkas " + c);
+        Debug.Log(name);
+        Debug.Log(integer);
+    }
+    
+    public int Method2()
+    {
+        return d = a + b;
+    }
+    
+    public int Method21(int e)
+    {
+        return d = e + b;
+    }
+
+    public void Method3()
+    {
+        int result = Method2(); // 0 = 1+ 2 ( a + b)
+        result = result + 1; // 3 = 3 + 1
+        Debug.Log(result); // 4
+        Debug.Log(Method2() +2 ); // 1 + 2 + 2
+        
+        Debug.Log(Method21(integer));
+    }
+
+    public void Meth1()
+    {
+        Debug.Log("Meth1");
+    }
+
+    private void Meth2()
+    {
+        Debug.Log("Meth2");
+    }
+
+    void Start()
+    {
+        // Method3();
+        uzd2("john");
+        uzd2("karen");
+        
+        uzd3(4, 5);
+        uzd3(5,6);
+        uzd3(653,643);
+        uzd3(523,325);
+
+        
+    }
+
+    private void uzd1()
+    {
+        Debug.Log("Welcome Friends!");
+        Debug.Log("Have a nice day!");
+    }
+    
+    private void uzd2(string name)
+    {
+        Debug.Log("inputed : " + name);
+    }
+    
+    private void uzd3(int x, int y)
+    {
+        Debug.Log(x + y);
+    }
+    
+}
+
+/*
+1. Write a program in C# Sharp to create a user define function.
+    ---Expected Output :
+Welcome Friends!
+    Have a nice day!
+
+2. Write a program in C# Sharp to create a user 
+    define function with parameters
+    ---Test Data : Please input a name : John
+    ---Expected Output :
+Welcome friend John !
+Have a nice day!
+
+    3. Write a program in C# Sharp to create a function 
+for the sum of two numbers.
+    --- Test Data : x : 15 y: 16
+    --- Expected Output : The sum of two numbers is : 31
+    
+*/
