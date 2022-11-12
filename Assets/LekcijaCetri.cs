900f72abb0159bcf46097e8df4d2f1556e4f33af
Eric <lil.reenas@gmail.com>
2022-11-12 15:07:03 +0200

    lekcija tris

---
+++ Assets/LekcijaCetri.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LekcijaCetri : MonoBehaviour
+{
+    void Start()
+    {
+        // MethodOne();
+        // MethodTwo();
+        // MethodThree();
+        // MethodSeven();
+        // MethodEight();
+        // MethodNine();
+        // MethodTen();
+        MethodEleven();
+    }
+
+    private void MethodOne()
+    {
+        int loop = 0;
+
+        while(loop < 5)
+        {
+            Debug.Log(loop);
+            loop++;
+            // loop +=2;
+        }
+    }
+
+    private void MethodTwo()
+    {
+        int loop = 0;
+
+        do
+        {
+            Debug.Log(loop);
+            loop++;
+        } while ( loop < 5);
+    }
+
+    private void MethodThree()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    private void MethodFour()
+    {
+        int i;
+
+        for(i = 0; i < 5; i++)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    private void MethodFive()
+    {
+        int i;
+        int lenght = 5;
+
+        for(i = 0; i < lenght; i++)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    private void MethodSix()
+    {
+        int i;
+        int lenght = 5;
+
+        for(i = 0; i < lenght; i += 2)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    private void MethodSeven()
+    {
+        for(int i = 5; i > 0; i--)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    private void MethodEight()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            if( i == 3)
+            {
+                continue;
+            }
+
+            Debug.Log(i);
+        }
+    }
+
+    private void MethodNine()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            if( i == 5)
+            {
+                break;
+            }
+
+            Debug.Log(i);
+        }
+    }
+
+    private void MethodTen()
+    {
+        int[] intBox = {0,2,5,6};
+        Debug.Log("Lenght: " + intBox.Length);
+
+        for(int i = 0; i < intBox.Length; i++)
+        {
+            Debug.Log(intBox[i]);
+        }
+
+        // Debug.Log(intBox[3]);
+    }
+
+    private void MethodEleven()
+    {
+        int[] intBox = {0,2,5,6};
+        Debug.Log("Lenght: " + intBox.Length);
+
+        foreach(int index in intBox)
+        {
+            Debug.Log(index);
+        }
+    }
+
+    private void MethodTwelve()
+    {
+        // List<int> intBox2 = new List<int>();
+        List<int> intBox = new List<int> {0,2,5,6};
+        Debug.Log("Lenght: " + intBox.Count);
+
+        foreach(int index in intBox)
+        {
+            Debug.Log(index);
+        }
+    }
+}
+
+/*
+1 . Print the numbers in the following format up to a given number .
+2 4 6 8 10( ar while loop)
+
+2. Write a program in C# Sharp to display the first 10 natural numbers. 
+
+Expected Output :
+1 2 3 4 5 6 7 8 9 10 ( ar for loop )
+
+
+3. Write a C# Sharp program to find the sum of first 10 natural numbers.
+
+Expected Output :
+The first 10 natural number is :
+1 2 3 4 5 6 7 8 9 10
+The Sum is : 55 ( ar for loop )
+*/
+
+/*
+ loop ++;
+ loop += 1;
+ loop = loop +1;
+
+ loop += 2;
+ loop = loop +2;
+
+*/
---
+++ Assets/LekcijaTris.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LekcijaTris : MonoBehaviour
+{
+    public Option option;
+
+    public enum Option
+    {
+        ONE,
+        TWO,
+        THREE
+    }
+
+    void Start()
+    {
+        // MethodFour(option);
+        MethodFive();
+    }
+
+    private void MethodOne()
+    {
+        int option = 0;
+
+        switch(option)
+        {
+            case 0 :
+                Debug.Log("case 0");
+            break;
+
+            case 1 :
+                Debug.Log("case 1");
+            break;
+
+            case 2 :
+                Debug.Log("case 2");
+            break;
+        }
+    }
+
+    private void MethodTwo()
+    {
+        int option = 3;
+
+        switch(option)
+        {
+            case 0 :
+                Debug.Log("case 0");
+            break;
+
+            case 1 :
+                Debug.Log("case 1");
+            break;
+
+            case 2 :
+                Debug.Log("case 2");
+            break;
+
+            default:
+                Debug.Log("Default");
+            break;
+        }
+    }
+
+    private void MethodThree()
+    {
+        string option = "A";
+
+        switch(option)
+        {
+            case "A" :
+                Debug.Log("case 0");
+            break;
+
+            case "safdaf" :
+                Debug.Log("case 1");
+            break;
+
+            case "b" :
+                Debug.Log("case 2");
+            break;
+        }
+    }
+
+    private void MethodFour(Option option)
+    {
+        switch(option)
+        {
+            case Option.ONE :
+                Debug.Log("case 0");
+            break;
+
+            case Option.TWO :
+                Debug.Log("case 1");
+            break;
+
+            case Option.THREE :
+                Debug.Log("case 2");
+            break;
+        }
+    }
+
+    private void MethodFive()
+    {
+        int option = 2;
+
+        switch(option)
+        {
+            case 0 :
+            case 1 :
+                Debug.Log("case 1");
+            break;
+
+            case 2 :
+                Debug.Log("case 2");
+            break;
+        }
+    }
+
+    private void UzdOne(int grade)
+    {
+        switch(grade)
+        {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                Debug.Log("F");
+                break;
+            case 5:
+                Debug.Log("D");
+                break;
+            case 6:
+                Debug.Log("C");
+                break;
+            case 7:
+            case 8:
+                Debug.Log("B");
+                break;
+            case 9:
+                Debug.Log("A");
+                break;
+            case 10:
+                Debug.Log("A+");
+                break;
+        }
+    }
+}
+
+/*
+    create function/method that calcualtes students grade from integer
+
+    10 - A+
+    9 - A
+    7,8 - B // NO 7 LIDZ 8
+    6 - C
+    5 - D
+    0,4 - F // NO 0 LIDZ 4
+*/
